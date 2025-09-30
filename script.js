93b137a618c539ef2dd2d248d8f31b2488dfcec4
SobarrSoFarr <dinnfavboyz@gmail.com>
2025-09-30 23:24:49 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// Contoh jika Anda ingin menambahkan fungsi tombol menu di masa depan
+// const menuIcon = document.querySelector('#menu-icon');
+// const navbar = document.querySelector('.navbar');
+
+// menuIcon.onclick = () => {
+//     menuIcon.classList.toggle('bx-x');
+//     navbar.classList.toggle('active');
+// };
+
+// Jika Anda ingin membuat logika untuk mengubah teks animasi secara manual
+// const animatedText = document.querySelectorAll('.home-info h2 span');
+// let textIndex = 0;
+
+// function changeText() {
+//    // Logika untuk menampilkan dan menyembunyikan teks
+// }
+// setInterval(changeText, 4000); 
+
+console.log("Skrip JavaScript Portofolio Anda telah dimuat.");
