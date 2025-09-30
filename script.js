e94767e4624d669b96010ace574a8d538d308674
SobarrSoFarr <dinnfavboyz@gmail.com>
2025-09-30 23:14:57 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Smooth Parallax
-const parallax = document.querySelector(".parallax-bg");
-
-window.addEventListener("scroll", () => {
-    let offset = window.scrollY;
-    parallax.style.transform = `translateY(${offset * 0.5}px)`; // 0.5 = kecepatan background
-});
